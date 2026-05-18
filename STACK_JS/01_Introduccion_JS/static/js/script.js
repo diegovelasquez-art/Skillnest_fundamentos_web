6a91b65494b2d010653cf9e08d1171919894ca6a
diegovelasquez-art <diegovelasquez@liceovvh.cl>
2026-05-18 15:32:44 -0400

    ok

---
+++ STACK_JS/01_Introduccion_JS/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("Prueba de conexion...");
+function cambiarTexto() {
+  let titulo = "Hola, Bienvenido";
+  if (document.getElementById("titulo").innerText == titulo) {
+    document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
+      document.getElementById("titulo").style.color= "red";
+  } else {
+    document.getElementById("titulo").innerText = titulo;
+      document.getElementById("titulo").style.color= "purple";
+  }
+}
